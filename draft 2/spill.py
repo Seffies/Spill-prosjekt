28c5cdbee2887154439221bbb5f102603d7ed3bf
Seffies <martinsefland@hotmail.com>
2023-11-14 11:11:50 +0100

    la til readme-fil

--- draft 2/spill.py
+++ draft 2/spill.py
@@ -15,3 +15,59 @@ class TTT():
     def __init__(self, x:int, y:int) -> None:
         self.x = x
         self.y = y
+
+        self.state = 0
+
+        self.spiller1avat = "X"
+        self.spiller1farg = "blue"
+        self.spiller1score = 1
+
+        self.spiller2avat = "O"
+        self.spiller2farg = "red"
+        self.spiller2score = -1
+
+        self.spiller0avat = ""
+        self.spiller0farg = "white"
+
+    def draw(self, vindu):
+        pg.draw.circle(vindu, self.spiller0farg, [self.x, self.y], 12)
+    
+    def sjekkPos(self, mus_x, mus_y, tur):
+        if self.x - 12 < mus_x < self.x +12 and self.y - 12 < mus_y < self.y + 12:
+            if self.state == 0:
+                if tur == 1:
+                    self.spiller0farg = self.spiller1farg
+                    self.state += self.spiller1score 
+                if tur == 2: 
+                    self.spiller0farg = self.spiller2farg
+                    self.state += self.spiller2score 
+                    
+
+lengde = 3
+
+
+for i in range(0,lengde):
+    for j in range(0,lengde):
+        spill.append(TTT(constx + j*30, consty + i *30))
+
+
+
+
+while True:
+    for hendelse in pg.event.get():
+        if hendelse.type == pg.QUIT:
+            pg.quit()
+            raise SystemExit
+        
+        mus_x, mus_y = pg.mouse.get_pos()
+
+        for i in range(len(spill)):
+            spill[i].sjekkPos(mus_x, mus_y, tur)
+
+            spill[i].draw(vindu)
+
+
+
+
+        pg.display.flip()
+        klokke.tick(FPS)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# spillprosjekt
+
+ganske mye grovere variant av tic tac toe
+
+todo: 
+- [ ] få inn avatar for spillere
+- [ ] fikse endring av tur
+- [ ] lage det store brettet
+- [ ] fikse vinn
